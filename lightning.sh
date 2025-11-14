7e1ef93b35d13cf8ceb7f4d79c031f42e29f23c0
bruce_d <dearingdbk@gmail.com>
2025-11-14 12:34:07 -0500

    upload lightning sensor script

--- lightning.sh
+++ lightning.sh
@@ -6,10 +6,25 @@
 
 knownfile="/home/raspy/Documents/lightning_data.txt"
 tmpfile=$(mktemp) || { printf '%s\n' "mktemp failed" >&2; exit 1; } #create a safe tmp file using mktemp to put our data.
+
 trap '{
-	[ -n "$tmpfile" ] && rm -f "$tmpfile"
-	[ -e /proc/$$/fd/3 ] && exec 3>&-
-}' EXIT #trap any exit to clean up our tmp file, and close fd.
+	# Remove temporary file if it exists
+    	if [ -n "$tmpfile" ] && [ -e "$tmpfile" ]; then
+		rm -f "$tmpfile"
+	fi
+
+    	# Restore terminal settings and close FD 3 if it is open
+	if exec 3>&3 2>/dev/null; then
+        	stty sane <&3 2>/dev/null
+        	exec 3>&-
+    	fi
+
+    	exit 0
+
+	#[ -n "$tmpfile" ] && rm -f "$tmpfile"
+	#[ -e /proc/$$/fd/3 ] && stty -icrnl <&3 2>/dev/null && stty flush <&3 2>/dev/null && exec 3>&-
+	#exit 0
+}' EXIT SIGINT SIGQUIT SIGTERM #trap any exit to clean up our tmp file, and close fd.
 
 if [ ! -t 0 ]; then  # check stdin for any data
 	cat >"$tmpfile"
@@ -25,7 +40,7 @@ else
 	exit 1
 fi
 
-port="/dev/ttyUSB4" #set port to the location of the usb to serial device
+port="/dev/ttyUSB0" #set port to the location of the usb to serial device
 baud=9600 # set the baud rate
 
 # If we need to set the data to rs485 not posix
