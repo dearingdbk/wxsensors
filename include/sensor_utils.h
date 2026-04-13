84023521170320b880c3cd0e9e3552b490970079
bruce_d <dearingdbk@gmail.com>
2026-04-13 12:14:10 -0400

    Update to sensor_utils.h, and sensor_utils.c removing any functions associated with the BTD-300 sensor, which have been ported to btd300_utils.c, and btd300_utils.h.

--- include/sensor_utils.h
+++ include/sensor_utils.h
@@ -49,22 +49,6 @@ char get_wind_units(int chk_val);
 
 /// FLASH SENSOR ///
 
-typedef struct {
-	uint16_t overhead;
-	uint16_t vicinity;
-	uint16_t near_distant;
-    uint16_t far_distant;
-	char serial_num[13];
-} flash_sensor;
-
-
-time_t parse_btd_datetime(const char *date_str, const char *time_str);
-int format_btd_datetime(time_t t, char *date_str, char *time_str);
-int update_btd_timestamps(const char *input, char *output, size_t output_size);
-int init_flash(flash_sensor **ptr);
-int reset_flash(flash_sensor **ptr);
-int set_dist(flash_sensor **ptr, const char *buf);
-
 /// END FLASH SENSOR ///
 
 #endif
